01c2643d20e47e8bde018bcd8cc9f9dd1da343a2
tsipib <tzipibarel4@gmail.com>
2022-02-05 18:32:16 +0200

    adding expense option

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,31 @@
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
+
+  const [filteredYear, setfilteredYear] = useState("2020");
+
+  const filterYearHandler = year => {
+    setfilteredYear(year);
+  };
+
   return (
-    <Card className="expenses">
-        {props.items.map(item => <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}></ExpenseItem>)}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onSelectFilter={filterYearHandler} />
+        {props.items.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          ></ExpenseItem>
+        ))}
+      </Card>
+    </div>
   );
 }
 

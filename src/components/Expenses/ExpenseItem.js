fb34160a49a03683905bd6c9e0e289880456ae34
tsipib <tzipibarel4@gmail.com>
2022-02-13 13:09:52 +0200

    remove change title btn-fix

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,7 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
     </Card>
